441e7123c3a237164dfe91cfb3c70e84f4b8e246
sneh prajapti <sneh.propelius@gmail.com>
2026-09-15 14:55:20 +0530

    save load and remove functionality done

--- todo.js
+++ todo.js
@@ -1,11 +1,14 @@
 let task = [];
 
 document.getElementById("btnadd").addEventListener("click",addTask);
+document.getElementById("btnclear").addEventListener("click",clearAll);
+document.getElementById("btnsave").addEventListener("click",save);
+document.getElementById("btnload").addEventListener("click",loadData);
 
 function displayTask(){
     let values = "";
-    task.forEach(function(val){
-        values+="<li>" + val + "</li>";
+    task.forEach(function(val,index){
+        values+="<li>" + val + " <button onclick='removeData("+index+")'> X</button>"+"</li>";
     });
     document.getElementById("list").innerHTML = values;
 };
@@ -21,3 +24,36 @@ function addTask(){
         displayTask();
     }
 };
+
+function clearAll(){
+    if(task.length==0){
+        alert("No task to clear!!");
+    }else{
+        save();
+        task = [];
+        displayTask();
+        alert("DATA CLEARED!!  YOU CAN LOAD DATA BACK WITH LOAD BUTTON");
+    }
+    
+}
+
+function save(){
+    if(task.length==0){
+        alert("No data to save!!");
+    }else{
+        localStorage.setItem("tasks",JSON.stringify(task));
+        alert("DATA SAVED SUCCESSFULLY")
+    }
+    
+}
+
+function loadData(){
+    let taskk = localStorage.getItem("tasks");
+    task = JSON.parse(taskk);
+    displayTask();
+}
+
+function removeData(i){
+    task.splice(i,1);
+    displayTask();
+}
