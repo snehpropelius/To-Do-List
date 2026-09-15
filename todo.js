5d42ebc70f1faae8d17fafa6d28aa2a57c653e06
sneh prajapti <sneh.propelius@gmail.com>
2026-09-15 13:21:28 +0530

    display and add task functionality

--- todo.js
+++ todo.js
@@ -1,2 +1,23 @@
 let task = [];
 
+document.getElementById("btnadd").addEventListener("click",addTask);
+
+function displayTask(){
+    let values = "";
+    task.forEach(function(val){
+        values+="<li>" + val + "</li>";
+    });
+    document.getElementById("list").innerHTML = values;
+};
+
+function addTask(){
+    let taskInput = document.getElementById("initem");
+    let text = taskInput.value;
+    if(text === ""){
+        alert("please fill the task first!!")
+    }else{
+        task.push(text);
+        taskInput.value="";
+        displayTask();
+    }
+};
